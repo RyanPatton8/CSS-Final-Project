b2209dd9d1af929e04d0f5831608400ee6dc182a
Ryan Patton <ryanpatton173@gmail.com>
2024-08-07 22:25:07 -0400

    I grinded to get this done

--- js/header-footer.js
+++ js/header-footer.js
@@ -1,6 +1,8 @@
+// get a reference to header and footer by id
 const mainHeader = document.querySelector("#main-header");
 const mainFooter = document.querySelector("#main-footer");
 
+// insert all of the elements i want inside them
 mainHeader.innerHTML = `
         <div class="header-logo-holder">
             <a href="index.html"><img src="./img/MainLogo.png" alt="Header main logo"></a>
@@ -11,40 +13,44 @@ mainHeader.innerHTML = `
               <li><a href="products.html">Products</a></li>
               <li><a href="contact-us.html">Contact</a></li>
               <li><a href="about-us.html">About Us</a></li>
-              <li><a href="#">C</a></li>
+              <li><a href="#"><img src="./img/shopping-cart.png" alt="shopping cart"></a></li>
             </menu>
         </nav>
-        <button class="burger">
-            <div></div>
-            <div></div>
-            <div></div>
-        </button>
+        <a href="#">
+            <button class="burger">
+                <div></div>
+                <div></div>
+                <div></div>
+            </button>
+        </a>
+        <a href="#" class="mobile-cart"><img src="./img/shopping-cart.png" alt="shopping cart"></a>
         <nav class="mobile-nav">
             <menu>
               <li><a href="index.html">Home</a></li>
               <li><a href="products.html">Products</a></li>
-              <li><a href="gloves.html">Contact</a></li>
-              <li><a href="#">About Us</a></li>
-              <li><a href="#">View Checkout</a></li>
+              <li><a href="contact-us.html">Contact</a></li>
+              <li><a href="about-us.html">About Us</a></li>
             </menu>
         </nav> 
 `
 
 mainFooter.innerHTML = `
         <div class="footer-logo-holder">
-            <a href="index.html"><img src="./img/MainLogo.png" alt="Header main logo"></a>
+            <a href="#"><img src="./img/MainLogo.png" alt="Header main logo"></a>
         </div>
         <div class="social-icons">
             <div>
-                <a href="index.html"><img src="./img/SocialMediaIcons/5282541_fb_social media_facebook_facebook logo_social network_icon.png" alt="facebook logo"></a>
-                <a href="index.html"><img src="./img/SocialMediaIcons/5282544_camera_instagram_social media_social network_instagram logo_icon.png" alt="instagram logo"></a>
+                <a href="contact-us.html"><img src="./img/SocialMediaIcons/5282541_fb_social media_facebook_facebook logo_social network_icon.png" alt="facebook logo"></a>
+                <a href="contact-us.html"><img src="./img/SocialMediaIcons/5282544_camera_instagram_social media_social network_instagram logo_icon.png" alt="instagram logo"></a>
             </div>
             <div>
-                <a href="index.html"><img src="./img/SocialMediaIcons/5282545_pin_pinterest_inspiration_pinterest logo_icon.png" alt="pintrest logo"></a>
-                <a href="index.html"><img src="./img/SocialMediaIcons/5282551_tweet_twitter_twitter logo_icon.png" alt="twitter logo"></a>
+                <a href="contact-us.html"><img src="./img/SocialMediaIcons/5282545_pin_pinterest_inspiration_pinterest logo_icon.png" alt="pintrest logo"></a>
+                <a href="contact-us.html"><img src="./img/SocialMediaIcons/5282551_tweet_twitter_twitter logo_icon.png" alt="twitter logo"></a>
             </div>
         </div> 
 `
+// get a reference to the burger and mobile nav
+// if the burger button gets clicked display the mobile nav
 var burger = document.getElementsByClassName("burger")[0];
 var mobileNav = document.getElementsByClassName("mobile-nav")[0];
 
--- js/home.js
+++ js/home.js
@@ -1,18 +1,18 @@
-// Get the modal
+//Gain access to the parent of the info / the form
 var modal = document.getElementById("booking-modal");
-// Get the button that opens the modal
+// Get the button that opens the form
 var btn = document.getElementById("modalBtn");
-// Get the <span> element that closes the modal
+// Get the <span> element that closes the form
 var span = document.getElementsByClassName("close")[0];
-// When the user clicks on the button, open the modal
+// When the user clicks on the button, open the form
 btn.onclick = function() {
     modal.style.display = "block";
 }
-// When the user clicks on <span> (x), close the modal
+// When the user clicks on <span> (x), close the form
 span.onclick = function() {
     modal.style.display = "none";
 }
-// When the user clicks anywhere outside of the modal, close it
+// When the user clicks anywhere outside of the form, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
